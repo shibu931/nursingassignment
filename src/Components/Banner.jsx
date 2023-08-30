d58793e2d1f5bdd5f27aa2625d7076e41cf5c8d1
Shippu <itzshippu@gmail.com>
2023-08-30 10:26:08 +0530

    asfdsgfdsg

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,40 +2,30 @@ import React, { useEffect } from 'react'
 import EmbedForm from './Form'
 
 export default function Banner() {
-    useEffect(()=>{
-            const script = document.createElement("script");
-    script.src = "https://assets.frms.link/widgets/mfwidget.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-        script.onload = () => {
-            new window.MFWidget('64ed91de9d13aad6fcad0588').init();
-        }
-    },[])
     return (
         <>
-            <div className="hero-banner position-relative">
-                <div className="container px-3 px-md-5">
+            <div className="hero-banner position-relative pb-5">
+                <div className="container-fluid px-3 px-md-5">
                     <div className="row">
-                        <div className="col-lg-7">
+                        <div className="col-lg-4">
                             <div className="mt-2 mt-md-5">
                                 <div className='d-flex flex-column justify-content-center'>
-                                    <h1 className='fw-500 display-5 mb-4 p-3 p-lg-0 mt-4 mb-lg-4 text-sm-start text-center'>Worried About Nursing Assignment?  Hire Nursing Assignment Expert</h1>
+                                    <h1 className='fw-500  title-text mb-4 p-3 p-lg-0 mt-4 mb-lg-4 text-sm-start text-center'>Worried About Nursing<br className='d-none d-lg-block'/> Assignment? Hire Nursing<br className='d-none d-lg-block'/>Assignment Expert</h1>
                                     <h2 className="fs-5 fw-light mb-4 p-2 p-lg-0 text-light text-sm-start text-center">Connect with highly qualified and experienced
                                         Nursing Assignment experts, writers for all assignment on nursing at best prices with upto 50% off.
                                     </h2>
-                                    <div className='my-3'>
-                                    <div id="64ed91de9d13aad6fcad0588" ></div>
-                                    </div>
                                 </div>
 
                             </div>
                         </div>
-                        <div className="col-lg-5">
+                        <div className="col-lg-4 d-none d-lg-block">
                             <div className='banner-img'>
                                 <span className='dots d-none d-lg-block'></span>
                                 <img src="./assets/img/nurse.webp" className='position-relative ' style={{ zIndex: "2" }} width="90%" alt="" />
                             </div>
+                        </div>
+                        <div className="col-lg-4 d-flex justify-content-center ps-0 ps-lg-3 pb-5 pb-lg-0 pt-2">
+                            <EmbedForm/>
                         </div>
                     </div>
                 </div>
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react'
 
 function EmbedForm() {
-    // const script = document.createElement("script");
-    // script.src = "https://assets.frms.link/widgets/mfwidget.js";
-    // script.async = true;
-
-    // document.body.appendChild(script);
-
-    window.onload = () => {
-      new window.MFWidget('64ed91de9d13aad6fcad0588').init();
-    }
-    return <div id="64ed91de9d13aad6fcad0588" ></div>;
+    useEffect(()=>{
+      const script = document.createElement("script");
+      script.src = "https://forms.app/static/embed.js";
+      script.async = true;
+  
+      document.body.appendChild(script);
+  
+      script.onload = () => {
+        var formsappForm = new window.formsapp('64edf9f987b11fad18feb978', 'standard', {"width":"100%","height":"500px","opacity":0});
+      }
+    },[])
+    return <div formsappId="64edf9f987b11fad18feb978" className='w-80'></div>;
 }
 
 export default EmbedForm;
